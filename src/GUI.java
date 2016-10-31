cf87e5f7c4991671abb3a98416a3254f15bb8379
b9007 <alejandro.bautista90@yahoo.com.mx>
2016-10-31 16:12:40 -0600

    added the button for another random message

--- src/GUI.java
+++ src/GUI.java
@@ -1,17 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class GUI {
+public class GUI implements ActionListener {
 
-    public void startGUI(){
 
-        MessageApp app = new MessageApp();
-        JFrame window = new JFrame();
-        JPanel panel = new JPanel();
-        JTextArea displayTheQuote = new JTextArea(10, 20);
-        JScrollPane scroller = new JScrollPane(displayTheQuote);
-        JButton loveButton = new JButton("Love");
-        JButton lifeButton = new JButton("Life");
+    MessageApp app = new MessageApp();
+    JFrame window = new JFrame();
+    JPanel panel = new JPanel();
+    JTextArea displayTheQuote = new JTextArea(10, 20);
+    JScrollPane scroller = new JScrollPane(displayTheQuote);
+    JButton button = new JButton("Next quote");
+
+
+    public void startGUI() {
 
         scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
@@ -20,6 +22,9 @@ public class GUI {
         displayTheQuote.setFont(new Font("Serif", Font.CENTER_BASELINE, 18));
 
         panel.add(scroller);
+        panel.add(button);
+
+        button.addActionListener(this);
         //Split the line to the boundaries of the window
         displayTheQuote.setLineWrap(true);
 
@@ -32,7 +37,20 @@ public class GUI {
         window.setTitle("Quote of the moment!");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
-
     }
 
+    public void actionPerformed(ActionEvent event){
+
+        displayTheQuote.setText(app.selectRandomQuote(app.getTextFromFile()));
+        displayTheQuote.setFont(new Font("Serif", Font.CENTER_BASELINE, 18));
+    }
+    
 }
+
+
+
+
+
+
+
+
